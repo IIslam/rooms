74c12e4fb7f3ff19059c1c792335418cea07d4ce
Mohammed Osama <mohammedosama@ieee.org>
2019-03-06 18:30:32 +0200

    validate the given data while updating reservation

--- server/app.js
+++ server/app.js
@@ -31,8 +31,7 @@ app.use((req, res, next) => {
     next(error)
 })
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.send({
+    res.status(error.status || 500).send({
         error: {
             message: error.message
         }
